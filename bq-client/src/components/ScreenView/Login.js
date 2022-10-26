3f9d9cd63bced8341b6e12eed52572a202f0ce73
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-10-26 18:39:14 -0500

    Agrega estilo css al componente de Login.js

--- bq-client/src/components/ScreenView/Login.js
+++ bq-client/src/components/ScreenView/Login.js
@@ -3,18 +3,24 @@ import '../Login.css';
 function Login() {
     return (
     <section className='component-login'>
-      <div> <img className='logo' src="https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg"></img></div>
-      <div className='form-div'>
-      <form className="form-login">
-          <label>
-            <input type="text" name="name" value="Ingrese el email" />
-          </label>
-          <label>
-            <input type="text" name="name" value="ingrese la contraseña"/>
-          </label>
-          <input type="submit" value="Iniciar sesión" />
+      {/* <picture style={{display: 'block'}}>
+        <img src='https://cdn.stockai.com/detail/1663811319856-57462341-9fb6-4c13-ad5e-06e05b4bf727-768.jpg'></img>
+      </picture>https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg
+      <div> */}
+      <div className='div-login'>
+        <div> <img className='logo' src='https://i.ibb.co/4dcV7Vp/burgerq.png'></img></div>
+        <form className="form-login">
+            <label>
+              <input type='email' name='name' placeholder= 'Ingrese tu email' />
+            </label>
+            <label>
+              <input type='password' name='name' placeholder='ingrese la contraseña'/>
+            </label>
+            <input className= 'button' type='submit' value='Iniciar sesión' />
         </form>
-        </div>
+      </div>
+        
+      {/* </div> */}
     </section>
         
     );
