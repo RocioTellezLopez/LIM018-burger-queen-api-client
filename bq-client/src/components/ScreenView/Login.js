8829e0691f94a5f1301ff714fd18bb98883974b3
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-10-26 11:35:29 -0500

    estilo de css login

--- bq-client/src/components/ScreenView/Login.js
+++ bq-client/src/components/ScreenView/Login.js
@@ -1,19 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
+import '../Login.css';
 function Login() {
     return (
-    <section>
-      <div> <img src="https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg"></img></div>
-      <form>
+    <section className='component-login'>
+      <div> <img className='logo' src="https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg"></img></div>
+      <div className='form-div'>
+      <form className="form-login">
           <label>
-            Correo:
-            <input type="text" name="name" />
+            <input type="text" name="name" value="Ingrese el email" />
           </label>
           <label>
-            Contraseña:
-            <input type="text" name="name" />
+            <input type="text" name="name" value="ingrese la contraseña"/>
           </label>
           <input type="submit" value="Iniciar sesión" />
         </form>
+        </div>
     </section>
         
     );
