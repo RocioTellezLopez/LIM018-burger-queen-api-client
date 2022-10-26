7a5ffd943f0e112a8b761cd17a51a39e2f194a21
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-10-26 09:56:36 -0500

    estructuración del login

--- bq-client/src/components/ScreenView/Login.js
+++ bq-client/src/components/ScreenView/Login.js
@@ -1,7 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
 function Login() {
     return (
-     <div>componente Login</div>
-      
+    <section>
+      <div> <img src="https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg"></img></div>
+      <form>
+          <label>
+            Correo:
+            <input type="text" name="name" />
+          </label>
+          <label>
+            Contraseña:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Iniciar sesión" />
+        </form>
+    </section>
+        
     );
   }
   
