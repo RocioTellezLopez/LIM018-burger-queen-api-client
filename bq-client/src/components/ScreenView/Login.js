9c0381594cf662b9f3baaee2e5746d7d49a41aba
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-10-27 11:40:51 -0500

    Agrega manejo de cambio de estado al los input e imprime el valor ingresado en consola

--- bq-client/src/components/ScreenView/Login.js
+++ bq-client/src/components/ScreenView/Login.js
@@ -1,29 +1,52 @@
 /* eslint-disable jsx-a11y/alt-text */
+import { useState } from 'react';
 import '../Login.css';
+
+
 function Login() {
-    return (
-    <section className='component-login'>
-      {/* <picture style={{display: 'block'}}>
-        <img src='https://cdn.stockai.com/detail/1663811319856-57462341-9fb6-4c13-ad5e-06e05b4bf727-768.jpg'></img>
-      </picture>https://i.ibb.co/SxtmtC1/Captura-de-pantalla-2022-10-26-094956.jpg
-      <div> */}
-      <div className='div-login'>
-        <div> <img className='logo' src='https://i.ibb.co/4dcV7Vp/burgerq.png'></img></div>
-        <form className="form-login">
-            <label>
-              <input type='email' name='name' placeholder= 'Ingrese tu email' />
-            </label>
-            <label>
-              <input type='password' name='name' placeholder='ingrese la contraseña'/>
-            </label>
-            <input className= 'button' type='submit' value='Iniciar sesión' />
-        </form>
-      </div>
-        
-      {/* </div> */}
-    </section>
-        
-    );
+
+  // Hook useState
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // funcion para el input email
+  const handleInputChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    setEmail(event.target.value);
+  }
+
+  // funcion para el input email
+  const handlePasswordChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    setPassword(event.target.value)
   }
   
-  export default Login;
+  // funcion para mostar los datos ingresados en consola.
+  const enviarDatos = (event) => {
+    event.preventDefault()
+    console.log(`Inputs -> ${email} ${password} `)
+  }
+
+  return (
+    <section className='component-login'>
+      {/* <Example /> */}
+      <div className='div-login'>
+        <div><img className='logo' src='https://i.ibb.co/4dcV7Vp/burgerq.png'></img> </div>
+        <form className="form-login" onSubmit={enviarDatos}>
+          <label>
+            <input type='text' name='name' placeholder='Ingrese tu email' onChange={handleInputChange} />
+          </label>
+          <label>
+            <input type='password' name='name' placeholder='ingrese la contraseña' onChange={handlePasswordChange}/>
+          </label>
+          <input className='button' type='submit' value='Iniciar sesión' />
+          
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
