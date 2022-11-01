89da18656b0ae129e592195f1fe2758ae84e20ce
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-10-31 20:02:14 -0500

    Modifica peticion fetch por axios y crea el componente Logo

--- bq-client/src/components/ScreenView/Login.js
+++ bq-client/src/components/ScreenView/Login.js
@@ -1,11 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { useState } from 'react';
+import Logo from '../staticComponents/Logo';
 import '../Login.css';
-// import axios from 'axios';
-import fetch from 'axios';
-
-
-
+import axios from 'axios';
 
 function Login() {
 
@@ -15,8 +12,6 @@ function Login() {
 
   // funcion para el input email
   const handleInputChange = (event) => {
-    // console.log(event.target.name)
-    // console.log(event.target.value)
     setEmail(event.target.value);
   }
 
@@ -28,27 +23,29 @@ function Login() {
   
   // funcion para mostar los datos ingresados en consola.
   const enviarDatos = (event) => {
-    event.preventDefault()
-    // axios.post('http://localhost:3000/auth', {
-    //   email: email,
-    //   password: password
-    // })
-    //   .then((res) => console.log('peticion ->', res.data))
-    // console.log(`Inputs -> ${email} ${password} `)
-
-    fetch.post('http://localhost:3000/auth', {
+    event.preventDefault();
+    
+    axios.post('http://localhost:3000/auth', {
       email: email,
       password: password
     })
-      .then((res) => console.log('fetch', res.data))
-      .catch((error) => console.log(error));
+      .then((res) => {
+        console.log('Inicio de sesión con exito!')
+        console.log('fetch', res.data) // Cambio de vista
+      })
+      .catch((error) => {
+        console.log('Error al iniciar sesión') // mensaje de error en Login
+        console.log('err', error)
+        const mensaje = document.getElementById('mensaje');
+        // mensaje.textContent = 'Error al iniciar sesión';
+      });
   }
 
   return (
     <section className='component-login'>
       {/* <Example /> */}
       <div className='div-login'>
-        <div><img className='logo' src='https://i.ibb.co/4dcV7Vp/burgerq.png'></img> </div>
+        <Logo className='logo' />
         <form className="form-login" onSubmit={enviarDatos}>
           <label>
             <input type='text' name='name' placeholder='Ingrese tu email' onChange={handleInputChange} />
@@ -57,7 +54,8 @@ function Login() {
             <input type='password' name='name' placeholder='ingrese la contraseña' onChange={handlePasswordChange}/>
           </label>
           <input className='button' type='submit' value='Iniciar sesión' />
-          
+          {/* <p id='mensaje'></p> */}
+
         </form>
       </div>
     </section>
